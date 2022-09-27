a062e330d2385fd296c907d14e39aa347de500de
Will Mee <professional@will.mee.name>
2022-09-27 08:08:55 -0700

    first model

--- capstone-idv/idv_capstone_william_mee.r
+++ capstone-idv/idv_capstone_william_mee.r
@@ -1,3 +1,4 @@
+library(caret)
 library(ggplot2)
 library(tidyverse)
 library(RColorBrewer)
@@ -21,8 +22,9 @@ colnames(abalone_df) <- c(
   'ShellWeight', 
   'Rings'
   )
-
 abalone_df$Sex <- as.factor(abalone_df$Sex)
+
+color_palette <- 'Set2'
 
 # Data exploration
 # 4176 entries
@@ -32,17 +34,29 @@ dim(abalone_df)
 by_sex_summary <- abalone_df %>%
   group_by(Sex) %>%
   summarize(
-    Count=n(), RingsMean=mean(Rings), RingsSd=sd(Rings),
+    Count=n(), 
+    RingsMean=mean(Rings), 
+    RingsSd=sd(Rings)
+  )
+
+# Correlations
+abalone_df %>%
+  group_by(Sex) %>%
+  summarize(
     RingsWholeWeightCor=cor(Rings, WholeWeight),
     RingsShuckedWeightCor=cor(Rings, ShuckedWeight),
     RingsVisceraWeightCor=cor(Rings, VisceraWeight),
-    RingsShellWeightCor=cor(Rings, ShellWeight)
+    RingsShellWeightCor=cor(Rings, ShellWeight),
+    RingsLengthCor=cor(Rings, Length),
+    RingsHeightCor=cor(Rings, Height),
+    RingsDiameterCor=cor(Rings, Diameter)
   )
-# from the correlation, it looks like the WholeWeight is a good feature. Also 
-# interesting that immature is more strongly correlated in all the features: these
-# abalone are presumably growing.
+# from the correlation, it looks like WholeWeight and Height are good features. Also 
+# interesting that immature set is more strongly correlated in all the features: these
+# abalone are growing.
 
-# ring histograms grouped by sex
+# ring histograms grouped by sex.
+# The better correlation with the infantile abalone is clear
 abalone_df %>%
   ggplot(aes(x=Rings, color=Sex)) +
   geom_histogram(binwidth=1, aes(fill=Sex), alpha=0.5) +
@@ -57,6 +71,33 @@ abalone_df %>%
 abalone_df %>%
   ggplot(aes(x=Rings, y=WholeWeight, color=Sex)) +
   geom_point(aes(fill=Sex), alpha=0.8) +
-  scale_colour_brewer(palette = 'Set2') +
-  labs(title='Abalone Ring Count vs Whole Weight Variance', x='Rings', y = 'Whole Weight') 
-  
+  scale_colour_brewer(palette = color_palette) +
+  labs(title='Abalone Ring Count vs Whole Weight', x='Rings', y = 'Whole Weight (g)') 
+
+# scatter plot of rings vs height
+# shows some outliers
+abalone_df %>%
+  ggplot(aes(x=Rings, y=Height, color=Sex)) +
+  geom_point(aes(fill=Sex), alpha=0.8) +
+  scale_colour_brewer(palette = color_palette) +
+  labs(title='Abalone Ring Count vs Height', x='Rings', y = 'Height (mm)') 
+
+# Split into train and test sets
+set.seed(51, sample.kind = 'Rounding')
+test_index <- createDataPartition(abalone_df$Rings, times = 1, p = 0.1, list = FALSE)
+# 419 samples
+abalone_test <- abalone_df[test_index, ]
+# 3757 samples
+abalone_train <- abalone_df[-test_index, ]  
+
+# error functions
+abalone_rmse <- function(y_hat) {
+  sqrt(sum((round(y_hat, digits=0) - abalone_test$Rings)^2)/length(y_hat))
+}
+
+# Train a first model using the caret package
+train_glm <- train(Rings ~ ., method = 'glm', data = abalone_train)
+y_hat_glm <- predict(train_glm, abalone_test, type = 'raw')
+
+# 2.147319
+abalone_rmse(y_hat_glm)
